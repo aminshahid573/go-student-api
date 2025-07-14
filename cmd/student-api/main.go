d2d7a9a2c99f5fefadbf26dab0bdebae7f02b25d
aminshahid573 <createa557733@gmail.com>
2025-07-14 19:49:59 +0545

    Implement HTTP server setup and graceful shutdown in student API; update config struct for address mapping

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/aminshahid573/student-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to Student Api")
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student api"))
+	})
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Server Started",
+		slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Faild to Start Server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting Down the Server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to ShutDown Server", slog.String("error", err.Error()))
+	}
+
+	//you can also do this
+
+	// if err != nil {
+	// 	slog.Error("Failed to ShutDown Server", slog.String("error", err.Error()))
+	// }
+
+	slog.Info("Server ShutDown Successfully")
+
 }
