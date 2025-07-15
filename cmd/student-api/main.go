9aa355c533be5b980f85d84df7d3a8f3dc49d9cc
aminshahid573 <createa557733@gmail.com>
2025-07-15 14:33:42 +0545

    Refactor storage handling in student API; implement SQLite storage and update student creation logic

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/aminshahid573/student-api/internal/config"
 	"github.com/aminshahid573/student-api/internal/http/handlers/student"
+	"github.com/aminshahid573/student-api/internal/storage/sqlite"
 )
 
 func main() {
@@ -19,10 +20,17 @@ func main() {
 	cfg := config.MustLoad()
 
 	//database setup
+	storage, err := sqlite.New(cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("Storage Initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
 	//setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("POST /api/students", student.New())
+	router.HandleFunc("POST /api/students", student.New(storage))
 
 	//setup server
 	server := http.Server{
--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -8,12 +8,13 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/aminshahid573/student-api/internal/storage"
 	"github.com/aminshahid573/student-api/internal/types"
 	"github.com/aminshahid573/student-api/internal/utils/response"
 	"github.com/go-playground/validator/v10"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("Creating a Student")
 		var student types.Student
@@ -39,6 +40,18 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
+		lastId, err := storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+		slog.Info("User Created Successfully", slog.String("userId", fmt.Sprint(lastId)))
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
 	}
 }
