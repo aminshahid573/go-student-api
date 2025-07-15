6307b5599fb0979f9f5d6133e94ce78388f3dd82
aminshahid573 <createa557733@gmail.com>
2025-07-15 15:52:03 +0545

    Add GET handler for retrieving all students; implement corresponding storage method in SQLite

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,6 +4,6 @@ import "github.com/aminshahid573/student-api/internal/types"
 
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
-
 	GetStudentById(id int64) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
